16d193f5dd32edaa00bdfcf1fa0568be3f1a4626
PickHD <taufikjanuar35@gmail.com>
2020-12-17 14:19:47 +0700

    try hash in ruby

---
+++ hash.rb
@@ -0,0 +1,14 @@
+
+puts Hash.new #( create blank hash {} )
+
+new_hash = Hash.new #( store new blank hash to variable global)
+
+#to access hash 
+
+grades = {"taufik"=>10,"januar"=>20}
+puts grades["januar"]
+
+# OR
+
+anotherGrades = {taufik: 10,januar: 20}
+puts anotherGrades[:taufik]
